3e25e2b7a83ec229ec1f73c37fd87278ac693313
Nauberth <lucasnauberth@gmail.com>
2023-08-01 15:32:55 -0300

    swiper added

--- floripe/src/App.js
+++ floripe/src/App.js
@@ -3,6 +3,8 @@ import icon from './assets/icon-yellow.svg';
 import spoilers from './assets/tittle1.svg';
 import arrowDown from './assets/arrow-down.svg';
 
+import { Carousel } from './components/carousel';
+
 import './styles/App.css';
 
 function App() {
@@ -16,6 +18,10 @@ function App() {
           <img src={icon} className='icon' alt="Ícone de expectativa"/>
           <img src={spoilers} className='spoilersText' alt="Spoilers"/>
           <img src={arrowDown} className='arrowDown' alt='Seta para baixo'/>
+
+          <h3 className='questionBeforeCarousel'>VOCÊ JÁ ESTAVA SABENDO?</h3>
+          <Carousel />
+
         </div>
        
       </body>
